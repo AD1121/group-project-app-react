deaef37d50febc4b78a21b48110b42b2e0c84869
AD1121 <sinagh30@gmail.com>
2023-03-25 14:35:54 +0100

    plugin and files added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const express = require('express')
 const router = express.Router()
+
 const {
   registerUser,
   loginUser,
   getMe
-} = require('../controllers/userController')
+} = require('../controllers/useController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
