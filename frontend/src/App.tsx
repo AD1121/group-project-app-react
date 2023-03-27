54dbaa2f93ce4b0cbb461f540a178958d9e3bce0
AD1121 <sinagh30@gmail.com>
2023-03-27 19:24:38 +0200

    files added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Userspage from './pages/Userspage';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <Homepage />
+     <Login />
+     <Userspage />
     </div>
   );
 }
