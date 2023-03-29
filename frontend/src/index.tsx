d6e109a639aee364e66792d5136310769da6a413
AD1121 <sinagh30@gmail.com>
2023-03-29 14:21:08 +0200

    design made

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,4 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
