500dd878f83a14bdafbb8c8a94214bee6c7332be
AD1121 <sinagh30@gmail.com>
2023-03-29 11:13:16 +0200

    login style made

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom'
 import './login.css'
 
 export default function Login() {
+
+  let navigate = useNavigate()
+
   return (
     <div className="center">
       <h1>Login</h1>
@@ -15,10 +19,11 @@ export default function Login() {
           <span></span>
           <label>Password</label>
         </div>
-        {/* <div className="pass">Forgot Password?</div> */}
+        <div className="pass">Forgot Password?</div>
         <input type="submit" value="Login" />
+        {/* <input type="submit" value="cancel" /> */}
         <div className="signup_link">
-          Already member? <a href="#">Login</a>
+          Not a  member? <a onClick={()=>navigate('/register')}>register</a> 
         </div>
       </form>
     </div>
