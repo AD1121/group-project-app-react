9ab98755fed8e539a12a2eb18cbc1858adeecabf
AD1121 <sinagh30@gmail.com>
2023-04-01 19:56:48 +0200

    Delete function added

--- frontend/src/components/BookingList.tsx
+++ frontend/src/components/BookingList.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import './bookingList.css'
-import axios from 'axios';
 
 interface IPost {
   _id: string,
@@ -34,11 +33,31 @@ export default function BookingList() {
     fetchPosts();
   })
 
+  const handleDelete = async (id: string) => {
+    
+    try {
+      const token = localStorage.getItem('accessToken');
+      
+      const response = await fetch(`/api/works/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        
+      })
+      const data = await response.json()
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div>
       <h3>Bookings:</h3>
       <ul className="booking-list">
-      {posts.map(booking => <li className='booking-li' key={booking._id}> {booking.calendar} <span> {booking.hours} </span> <span> {booking.cleanerName} </span> <span> {booking.level} </span> <button className='delete'><i className="fa-solid fa-trash"></i></button> </li>)}
+      {posts.map(booking => <li className='booking-li' key={booking._id}> {booking.calendar} <span> {booking.hours} </span> <span> {booking.cleanerName} </span> <span> {booking.level} </span> <button className='delete' onClick={() => handleDelete(booking._id)}><i className="fa-solid fa-trash"></i></button> </li>)}
       </ul>
     </div>
   )
