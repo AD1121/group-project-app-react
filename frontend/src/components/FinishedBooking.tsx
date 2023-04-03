f20415d9760873c921b565d141510227c17ead98
AD1121 <sinagh30@gmail.com>
2023-04-03 10:55:17 +0200

    fnished component done

--- frontend/src/components/FinishedBooking.tsx
+++ frontend/src/components/FinishedBooking.tsx
@@ -2,104 +2,75 @@ import { useEffect, useState } from 'react'
 import './bookingList.css'
 
 interface IBookings {
+  id: number,
   cleanerName: string,
   calendar: string,
   hours: string,
   level: string
 }
 
-// const finishedBookings: Array<any> = [
-//   {
-//       id: 1,
-//       cleanerName: 'Erik',
-//       calendar: '16/04/2023',
-//       hours: '10:35',
-//       level: 'Top'
-//   },
-//   {
-//       id: 2,
-//       cleanerName: 'Lisa',
-//       calendar: '17/04/2023',
-//       hours: '12:55',
-//       level: 'Window'
-//   },
-//   {
-//       id: 3,
-//       cleanerName: 'Linda',
-//       calendar: '22/04/2023',
-//       hours: '20:35',
-//       level: 'Basic'
-//   },
-//   {
-//       id: 4,
-//       cleanerName: 'Charles',
-//       calendar: '22/04/2023',
-//       hours: '08:35',
-//       level: 'Diamont'
-//   },
-//   {
-//       id: 5,
-//       cleanerName: 'Lisa',
-//       calendar: '16/04/2023',
-//       hours: '18:35',
-//       level: 'Basic'
-//   }
-// ]
-
 export default function FinishedBooking() {
+
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
   const [bookings, setBookings] = useState<IBookings[]>([{
+    id: 1,
     cleanerName: 'Erik',
     calendar: '16/04/2023',
     hours: '10:35',
     level: 'Top'
 },
 {
+    id: 2,
     cleanerName: 'Lisa',
     calendar: '17/04/2023',
     hours: '12:55',
     level: 'Window'
 },
 {
+    id: 3,
     cleanerName: 'Linda',
     calendar: '22/04/2023',
     hours: '20:35',
     level: 'Basic'
 },
 {
+    id: 4,
     cleanerName: 'Charles',
     calendar: '22/04/2023',
     hours: '08:35',
     level: 'Diamont'
 },
 {
+    id: 5,
     cleanerName: 'Lisa',
     calendar: '16/04/2023',
     hours: '18:35',
     level: 'Basic'
-}]);
+}])
 
-const [selectedItems, setSelectedItems] = useState<number[]>([])
-
-function handleItemSelect(index: number) {
-  setSelectedItems(selectedItems => {
-    if (selectedItems.includes(index)) {      
-      return selectedItems.filter(itemIndex => itemIndex !== index)
+  function handleSelectItem(id: number) {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter(item => item !== id))
     } else {
-      return [...selectedItems, index]
+      setSelectedItems([...selectedItems, id])
     }
-  })
-}
+  }  
 
-function handleDelete() {
-  setBookings(items => items.filter((item, index) => !selectedItems.includes(index)));
-  setSelectedItems([]);
-}
+  function handleDelete() {
+    console.log(selectedItems)
+    if (selectedItems.length === 0) {
+      setBookings([])
+    }else {
+      setBookings(bookings.filter(item => !selectedItems.includes(item.id)))
+      setSelectedItems([])
+    }
+  }
 
   return (
     <div>
       <h3>Finished Bookings:</h3>
       <ul className="booking-list">
-      {bookings.map((booking, index) => <li className='booking-li' key={index}> <input type="checkbox" onChange={() => handleItemSelect(index)}/> {booking.calendar} <span> {booking.hours} </span> <span> {booking.cleanerName} </span> <span> {booking.level} </span> </li>)}
+      {bookings.map(booking => <li className='booking-li' key={booking.id}> <input type="checkbox" onChange={() => handleSelectItem(booking.id)}/> {booking.calendar} <span> {booking.hours} </span> <span> {booking.cleanerName} </span> <span> {booking.level} </span> </li>)}
       </ul>
       <button className='btn-delete' onClick={handleDelete}>Delete All</button>
     </div>
